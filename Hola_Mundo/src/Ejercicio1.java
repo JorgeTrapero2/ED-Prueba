46099064d4b88a56b57baab5a15748c860eafdc8
jorge <jorge@DESKTOP-UO438BE>
2020-01-29 16:07:15 +0100

    ejercicio1

---
+++ Hola_Mundo/src/Ejercicio1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ejercicio1 {
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Introduzca una palabra: ");
+        String salida = "";
+        String palabra = in.nextLine();        
+        for (int i = 0; i < palabra.length(); i++) {
+            salida = palabra.charAt(i) + salida;                
+            }
+        if(palabra.equals(salida)){
+            System.out.println("si");
+        }else{
+            
+        }
+}
+}
